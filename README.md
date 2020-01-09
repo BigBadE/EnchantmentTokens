062e29a721aa457dc8db1ceb7b00dee45d1a3e38
BigBadE <ethan.menell@gmail.com>
2020-01-08 20:41:18 -0600

    Updated events

---
+++ README.md
@@ -0,0 +1,26 @@
+EnchantmentTokens is a plugin allowing for easy addition of custom enchantments into Minecraft.
+
+Current features:
+- Add enchantments to items. Currently supports:
+    - Armor
+    - Swords
+    - Bows
+- Future additions:
+    - Tridents
+    - Crossbows
+- Allows for enchantments to be bought through:
+    - Command GUI
+    - Signs
+- Currently supports the events:
+    - All:
+        - Equip
+        - Unequip
+        - Enchanted
+    - Pickaxe:
+        - Block break
+        - Block damage
+        
+    - Armor:
+        
+    - Sword:
+        
--- src/main/java/bigbade/enchantmenttokens/utils/MaterialGroupUtils.java
+++ src/main/java/bigbade/enchantmenttokens/utils/MaterialGroupUtils.java
@@ -4,4 +4,12 @@ import org.bukkit.Material;
 
 public class MaterialGroupUtils {
     public static final Material[] pickaxes = new Material[] { Material.WOODEN_PICKAXE, Material.STONE_PICKAXE, Material.IRON_PICKAXE, Material.GOLDEN_PICKAXE, Material.DIAMOND_PICKAXE };
+    public static final Material[] swords = new Material[] { Material.WOODEN_SWORD, Material.STONE_SWORD, Material.IRON_SWORD, Material.GOLDEN_SWORD, Material.DIAMOND_SWORD };
+    public static final Material[] axes = new Material[] { Material.WOODEN_AXE, Material.STONE_AXE, Material.IRON_AXE, Material.GOLDEN_AXE, Material.DIAMOND_AXE };
+    public static final Material[] hoes = new Material[] { Material.WOODEN_HOE, Material.STONE_HOE, Material.IRON_HOE, Material.GOLDEN_HOE, Material.DIAMOND_HOE };
+    public static final Material[] leather_armor = new Material[] { Material.LEATHER_HELMET, Material.LEATHER_CHESTPLATE, Material.LEATHER_LEGGINGS, Material.LEATHER_BOOTS };
+    public static final Material[] chainmain_armor = new Material[] { Material.LEATHER_HELMET, Material.LEATHER_CHESTPLATE, Material.LEATHER_LEGGINGS, Material.LEATHER_BOOTS };
+    public static final Material[] iron_armor = new Material[] { Material.CHAINMAIL_HELMET, Material.CHAINMAIL_CHESTPLATE, Material.CHAINMAIL_LEGGINGS, Material.CHAINMAIL_BOOTS };
+    public static final Material[] gold_armor = new Material[] { Material.GOLDEN_HELMET, Material.GOLDEN_CHESTPLATE, Material.GOLDEN_LEGGINGS, Material.GOLDEN_BOOTS };
+    public static final Material[] diamond_armor = new Material[] { Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE, Material.DIAMOND_LEGGINGS, Material.DIAMOND_BOOTS };
 }

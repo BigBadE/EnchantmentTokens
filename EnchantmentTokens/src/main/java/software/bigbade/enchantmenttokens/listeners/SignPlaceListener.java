34503ff358d83e7562c0285758f1c24e5b3b330c
bigbade <bigbade333@gmail.com>
2020-02-21 10:40:47 -0800

    Removed  license headers since this will be private

--- EnchantmentTokens/src/main/java/software/bigbade/enchantmenttokens/listeners/SignPlaceListener.java
+++ EnchantmentTokens/src/main/java/software/bigbade/enchantmenttokens/listeners/SignPlaceListener.java
@@ -1,22 +1,5 @@
 package software.bigbade.enchantmenttokens.listeners;
 
-/*
-EnchantmentTokens
-Copyright (C) 2019-2020 Big_Bad_E
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 import software.bigbade.enchantmenttokens.api.EnchantmentBase;
 import software.bigbade.enchantmenttokens.localization.TranslatedMessage;
 import software.bigbade.enchantmenttokens.utils.enchants.EnchantmentHandler;
@@ -43,7 +26,6 @@ public class SignPlaceListener implements Listener {
     }
 
     private boolean updateSign(EnchantmentBase base, SignChangeEvent event) {
-        System.out.println(base.getName() + " tested against " + event.getLine(1));
         if (base.getName().equalsIgnoreCase(event.getLine(1))) {
             event.getPlayer().sendMessage(TranslatedMessage.translate("enchantment.add", base.getName()));
             event.setLine(0, "[" + TranslatedMessage.translate( "enchantment") + "]");
